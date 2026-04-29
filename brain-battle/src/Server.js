957e27e3e9c72a10a9c90f461b20d06743f9670d
Ayauelemes <dana26032007@icloud.com>
2026-04-29 15:29:55 +0500

    update

--- brain-battle/src/Server.js
+++ brain-battle/src/Server.js
@@ -4,8 +4,8 @@ const { Pool } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 const host = "127.0.0.1";
 
